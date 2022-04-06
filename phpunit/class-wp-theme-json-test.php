e17d45918347170432830471cde3337ab9a7938b
André <583546+oandregal@users.noreply.github.com>
2022-04-06 19:28:08 +0200

    Add unit test for desired behaviour

--- phpunit/class-wp-theme-json-test.php
+++ phpunit/class-wp-theme-json-test.php
@@ -2451,4 +2451,24 @@ class WP_Theme_JSON_Gutenberg_Test extends WP_UnitTestCase {
 		$this->assertEqualSetsWithIndex( $expected, $actual );
 	}
 
+	function test_export_data_sets_appearance_tools() {
+		$theme = new WP_Theme_JSON_Gutenberg(
+			array(
+				'version'  => 2,
+				'settings' => array(
+					'appearanceTools' => true,
+				),
+			)
+		);
+
+		$actual   = $theme->get_data();
+		$expected = array(
+			'version'  => 2,
+			'settings' => array(
+				'appearanceTools' => true,
+			),
+		);
+
+		$this->assertEqualSetsWithIndex( $expected, $actual );
+	}
 }
